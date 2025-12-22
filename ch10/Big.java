263de283c57ec261276ac9226c418732b6cdda14
SimoneChimirri <simonechimirri2004@gmail.com>
2025-12-22 15:36:57 +0100

    ch10p2

--- ch10/Big.java
+++ ch10/Big.java
@@ -1,16 +1,44 @@
+import java.math.BigInteger;
+
 public class Big {
 
     /**
      * Computes the factorial function
-     * of a given integer n.
+     * of a given BigInteger n.
      * @param n the argument of the factorial function
      * @return the value of the factorial function
      */
-    public static int factorial(int n){
-        int result = 1;
-        for(int i=n;i > 1; i--){
-            result *= i;
+    public static BigInteger factorial(BigInteger n){
+        BigInteger result = BigInteger.valueOf(1);
+        for(BigInteger i = n; i.compareTo(BigInteger.ONE) > 0; i = i.subtract(BigInteger.ONE)){
+            result = result.multiply(i);
         }
         return result;
     }
+
+    public static void main(String[] args){
+        int x = 17;
+        BigInteger big = BigInteger.valueOf(x);
+        BigInteger small = BigInteger.valueOf(17);
+        BigInteger bigger = BigInteger.valueOf(1700000000);
+        BigInteger total = small.add(bigger);
+        BigInteger power = small.pow(x);
+        BigInteger product = small.multiply(big);
+        System.out.println(big);
+        System.out.println(small);
+        System.out.println(bigger);
+        System.out.println(total);
+        System.out.println(power);
+        System.out.println(product);
+
+        for(BigInteger i = BigInteger.ONE; i.compareTo(BigInteger.valueOf(31)) < 0; i = i.add(BigInteger.ONE)){
+            System.out.print(i + "\t");
+            System.out.print(factorial(i));
+            System.out.println();
+        }
+    }
 }
+
+
+
+
--- ch10/Pow.java
+++ ch10/Pow.java
@@ -1,3 +1,5 @@
+import java.math.BigInteger;
+
 /**
  * BigInteger exercise.
  */
@@ -19,6 +21,30 @@ public class Pow {
         } else {
             return t * t * x;
         }
+    }
+
+    /**
+     * BigInteger exponentiation.
+     */
+    public static BigInteger powBig(int x, int n) {
+        if (n == 0) return BigInteger.ONE;
+
+        // find x to the n/2 recursively
+        BigInteger t = powBig(x, n / 2);
+
+        // if n is even, the result is t squared
+        // if n is odd, the result is t squared times x
+        if (n % 2 == 0) {
+            return t.multiply(t);
+        } else {
+            return t.multiply(t).multiply(BigInteger.valueOf(x));
+        }
+    }
+
+    public static void main(String[] args){
+        System.out.println(pow(4,5));
+        System.out.println(powBig(4,5));
+        System.out.println(powBig(4,123));
     }
 
 }
